b341bdc113ba14d0d7e92932e04441803b497e21
Léo Peltier <contact@leo-peltier.fr>
2023-12-31 23:02:10 +0100

    Fix demo_follow

--- wasmsrc/demo_follow.c
+++ wasmsrc/demo_follow.c
@@ -13,7 +13,7 @@ EXPORT int32_t on_fire(
     use_type_t use_type,
     float value
 ) {
-    if (ent_matches(caller, NULL, "_wasm_make_follow") == 0) {
+    if (ent_matches(caller, "func_button", "follow")) {
         make_follow("barney");
         console_log(log_debug, "Made Barney follow us.");
         return true;
