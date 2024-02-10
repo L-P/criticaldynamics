ea61de5de9beaa164ec81e3f9d90cbcbd37f5ee2
Léo Peltier <contact@leo-peltier.fr>
2024-02-10 13:44:49 +0100

    Generator sequence

--- wasmsrc/c0a4.c
+++ wasmsrc/c0a4.c
@@ -9,7 +9,8 @@
 #include "lib/format.h"
 
 typedef struct{
-    bool generatorIsRunning;
+    uint32_t generatorRunningCount;
+    bool pressureIsUp;
     bool lightIsOn;
 } state_t;
 
@@ -18,10 +19,15 @@ static state_t state = {};
 EXPORT void on_activate() {
     console_logf(
         log_debug,
-        "state: generatorIsRunning(%d), lightIsOn(%d)\n",
-        state.generatorIsRunning,
+        "state: pressureIsUp(%d), generatorRunningCount(%d), lightIsOn(%d)\n",
+        state.pressureIsUp,
+        state.generatorRunningCount,
         state.lightIsOn
     );
+}
+
+bool generator_is_running() {
+    return state.generatorRunningCount >= 2;
 }
 
 EXPORT int32_t on_fire(
@@ -30,20 +36,41 @@ EXPORT int32_t on_fire(
     use_type_t use_type,
     float value
 ) {
-    if (ent_matches(caller, NULL, "generator_is_running")) {
-        if (!state.generatorIsRunning) {
-            // Small red light above the breaker.
-            ent_fire("biglight_ready", use_on, 0);
+    if (ent_matches(caller, NULL, "pressure_is_up")) {
+        if (!state.pressureIsUp) {
+            ent_fire("pressure_dials", use_on, 0);
+            state.pressureIsUp = true;
+            return true;
+        }
+    }
+
+    if (   ent_matches(caller, NULL, "generator_button_a")
+        || ent_matches(caller, NULL, "generator_button_b")
+    ) {
+        if (state.pressureIsUp) {
+            state.generatorRunningCount += 1;
+
+            if (ent_matches(caller, NULL, "generator_button_a")) {
+                ent_fire("generator_sound_a", use_on, 0);
+                ent_fire("pressure_dials_gen_a", use_on, 0);
+            } else if (ent_matches(caller, NULL, "generator_button_b")) {
+                ent_fire("generator_sound_b", use_on, 0);
+                ent_fire("pressure_dials_gen_b", use_on, 0);
+            }
         }
 
-        state.generatorIsRunning = true;
+        if (generator_is_running()) {
+            // Small red light above the breaker.
+            ent_fire("biglight_ready", use_on, 0);
+            ent_fire("generator_up_lights", use_on, 0);
+        }
         return true;
     }
 
     if (ent_matches(caller, "func_button", "biglight_lever_button")) {
         ent_fire("biglight_lever", use_toggle, 0);
 
-        if (state.generatorIsRunning && !state.lightIsOn) {
+        if (generator_is_running() && !state.lightIsOn) {
             ent_fire("biglight", use_on, 0);
             ent_fire("biglight_ready", use_off, 0);
             ent_fire("biglight_clang", use_toggle, 0);
@@ -52,6 +79,19 @@ EXPORT int32_t on_fire(
 
         return true;
     }
+
+    return false;
+}
+
+EXPORT int32_t on_master_check(const entity_t* activator, const entity_t* caller) {
+    if (   ent_matches(caller, NULL, "generator_button_a")
+        || ent_matches(caller, NULL, "generator_button_b")
+    ) {
+        return state.pressureIsUp;
+    }
+
+    console_log(log_error, "Unhandled master check for caller: ");
+    ent_print(log_error, caller);
 
     return false;
 }
